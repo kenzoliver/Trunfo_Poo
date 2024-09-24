1b4c89aa62fac2a32dad8b941e07fc82f2f3ae4d
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-24 10:43:08 -0300

    add class colors

--- trunfo/Game.java
+++ trunfo/Game.java
@@ -2,7 +2,8 @@ package trunfo;
 
 public class Game {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("          WELCOME TO GAME          ");
         Player p1 = new Player("Jean");
     }
 }
--- trunfo/Player.java
+++ trunfo/Player.java
@@ -4,10 +4,11 @@ public class Player{
     private String name;
     private Card[] deck = new Card[11];
 
-    public Player(String name){
+    public Player(String name)  throws InterruptedException {
         this.name = name;
         for (int i = 0; i < 11; i++) {
             deck[i] = new Card(); 
+            Thread.sleep(2000);
             deck[i].getAllAtributes();
         }
     }
