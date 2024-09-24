49884a04fe75d8f55b92d90256591d45d82d520c
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-24 15:27:16 -0300

    user game finish

--- trunfo/Game.java
+++ trunfo/Game.java
@@ -17,37 +17,38 @@ public class Game {
         player_name = read.nextLine();
         Player p1 = new Player(player_name);
         
-        while (i < 11) {
+        while (i < 11 | p1.getPoints() > 6 | p2.getPoints() > 6  ) {
             String choice;
             int player, com;
             // player game
-            try {
-                p1.getCard(i);
-                System.out.printf("Escolha um atributo:");
-                choice = read.nextLine();
-                player = p1.getAtribute(i, choice);
-                com = p2.getAtribute(i, choice);
-                if (player > com) {
-                    p1.addPoint();
-                    System.out.println(Colors.ANSI_GREEN_BACKGROUND + player_name + " ganhou essa rodada"+ Colors.ANSI_RESET + "\n");
+            while(true){
+                try {
+                    p1.getCard(i);
+                    System.out.printf("Escolha um atributo:");
+                    choice = read.nextLine();
+                    player = p1.getAtribute(i, choice);
+                    com = p2.getAtribute(i, choice);
+                    if (player > com) {
+                        p1.addPoint();
+                        System.out.println(Colors.ANSI_GREEN_BACKGROUND + player_name + " ganhou essa rodada"+ Colors.ANSI_RESET + "\n");
+                    }
+                    if (player < com) {
+                        p2.addPoint();
+                        System.out.println(Colors.ANSI_RED_BACKGROUND + "COM ganhou essa rodada" + Colors.ANSI_RESET + "\n");
+                    }
+                    if (player == com) {
+                        System.out.println(Colors.ANSI_YELLOW_BACKGROUND + "Empate" + Colors.ANSI_RESET + "\n");
+                    }
+                    System.out.println(Colors.ANSI_YELLOW_BACKGROUND +"          Placar          "+ Colors.ANSI_RESET);
+                    System.out.println(Colors.ANSI_GREEN+"Player:"+p1.getPoints()+Colors.ANSI_RESET+"  "+Colors.ANSI_RED+"COM:"+p2.getPoints()+Colors.ANSI_RESET);
+                    i += 1;
+                    break;
                 }
-                if (player < com) {
-                    p2.addPoint();
-                    System.out.println(Colors.ANSI_RED_BACKGROUND + "COM ganhou essa rodada" + Colors.ANSI_RESET + "\n");
+                catch (Exception e) {
+                    System.out.println("Este não é um atributo, digite como está na carta " + e.getMessage());
                 }
-                if (player == com) {
-                    System.out.println(Colors.ANSI_YELLOW_BACKGROUND + "Empate" + Colors.ANSI_RESET + "\n");
-                }
-                i += 1;
-
-                System.out.println(Colors.ANSI_YELLOW_BACKGROUND +"          Placar          "+ Colors.ANSI_RESET);
-                System.out.println(Colors.ANSI_GREEN+"Player:"+p1.getPoints()+Colors.ANSI_RESET+"  "+Colors.ANSI_RED+"COM:"+p2.getPoints()+Colors.ANSI_RESET);
-
             }
-            catch (Exception e) {
-                System.out.println("Este não é um atributo, digite como está na carta " + e.getMessage());
-            }
-            // COM game
+            
 
         }
      }
