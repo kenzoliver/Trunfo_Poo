7588987f2b5f8ceb2459bb639821700f85ed58b2
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-24 18:06:26 -0300

    automatized game

--- trunfo/Game.java
+++ trunfo/Game.java
@@ -13,11 +13,11 @@ public class Game {
         Player p2 = new Player("COM");
 
         System.out.println(Colors.ANSI_YELLOW_BACKGROUND + "          WELCOME TO GAME          " + Colors.ANSI_RESET);
-        System.out.printf("Informe o nome do Jogador:");
+        System.out.printf("Enter the Player's name:");
         player_name = read.nextLine();
         Player p1 = new Player(player_name);
 
-        while (i < 11 && p1.getPoints() < 5 && p2.getPoints() < 5) {
+        while (i < 10 && p1.getPoints() < 5 && p2.getPoints() < 5) {
             clearConsole();
             String choice;
             int player, com;
@@ -26,8 +26,13 @@ public class Game {
                 try {
                     p1.getCard(i);
                     Thread.sleep(1000);
-                    System.out.printf("Escolha um atributo:");
-                    choice = read.nextLine();
+
+                    // System.out.printf("Choose an attribute:");
+                    // choice = read.nextLine();
+                    System.out.println(player_name+" is choosing an attribute...");
+                    choice = p1.getBestAttribute(i);
+                    Thread.sleep(5000);
+                    System.out.println(player_name+" Chose:" + choice);
                     player = p1.getAtribute(i, choice);
                     com = p2.getAtribute(i, choice);
                     delaygame();
@@ -37,17 +42,17 @@ public class Game {
                     i += 1;
                     break;
                 } catch (Exception e) {
-                    System.out.println("Este não é um atributo, digite como está na carta " + e.getMessage());
+                    System.out.println("This is Not a attribute, type as it is in the letter " + e.getMessage());
                     Thread.sleep(3000);
                     clearConsole();
                 }
             }
             // COM game
             p2.getCard(i);
-            System.out.println("COM está escolhendo um atributo...");
+            System.out.println("COM is choosing an attribute...");
             Thread.sleep(5000);
             choice = p2.getBestAttribute(i);
-            System.out.println("COM escolheu: " + choice);
+            System.out.println("COM chose: " + choice);
             player = p1.getAtribute(i, choice);
             com = p2.getAtribute(i, choice);
             delaygame();
@@ -62,21 +67,21 @@ public class Game {
     private static void displayWinner(Player p1, Player p2, String playerName) {
         if (p1.getPoints() > p2.getPoints()) {
             System.out.println(
-                    Colors.ANSI_GREEN_BACKGROUND + Colors.ANSI_BLACK + playerName + " Venceu!" + Colors.ANSI_RESET);
+                    Colors.ANSI_GREEN_BACKGROUND + Colors.ANSI_BLACK + playerName + " WIN!" + Colors.ANSI_RESET);
         } else {
-            System.out.println(Colors.ANSI_RED_BACKGROUND + Colors.ANSI_WHITE + "COM Venceu!" + Colors.ANSI_RESET);
+            System.out.println(Colors.ANSI_RED_BACKGROUND + Colors.ANSI_WHITE + "COM WIN!" + Colors.ANSI_RESET);
         }
     }
 
     private static void determineRoundWinner(Player p1, Player p2, int playerAttr, int comAttr, String playerName) {
         if (playerAttr > comAttr) {
             p1.addPoint();
-            System.out.println(Colors.ANSI_GREEN_BACKGROUND + playerName + " ganhou essa rodada" + Colors.ANSI_RESET);
+            System.out.println(Colors.ANSI_GREEN_BACKGROUND + playerName + " Win this round" + Colors.ANSI_RESET);
         } else if (playerAttr < comAttr) {
             p2.addPoint();
-            System.out.println(Colors.ANSI_RED_BACKGROUND + "COM ganhou essa rodada" + Colors.ANSI_RESET);
+            System.out.println(Colors.ANSI_RED_BACKGROUND + "COM Win this round" + Colors.ANSI_RESET);
         } else {
-            System.out.println(Colors.ANSI_YELLOW_BACKGROUND + "Empate" + Colors.ANSI_RESET);
+            System.out.println(Colors.ANSI_YELLOW_BACKGROUND + "Draw" + Colors.ANSI_RESET);
         }
 
     }
