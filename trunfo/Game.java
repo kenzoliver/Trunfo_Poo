c0da25fc9f14acdfc382b6755b759ae637dd86f0
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-24 12:30:27 -0300

    function check who won

--- trunfo/Game.java
+++ trunfo/Game.java
@@ -6,21 +6,39 @@ public class Game {
 
     public static void main(String[] args) throws InterruptedException {
         Scanner read = new Scanner(System.in);
+        
+        int i = 0;
         String player_name;
+
+        Player p2 = new Player("COM");
+        
         System.out.println(Colors.ANSI_YELLOW_BACKGROUND + "          WELCOME TO GAME          " + Colors.ANSI_RESET);
         System.out.printf("Informe o nome do Jogador:");
         player_name = read.nextLine();
         Player p1 = new Player(player_name);
-        Player p2 = new Player("COM");
-
-        for (int i = 0; i < 11; i++) {
+        
+        while (true) {
             String choice;
+            int player, com;
+
             p1.getCard(i);
             System.out.printf("Escolha um atributo:");
             choice = read.nextLine();
 
             try { 
-                System.out.println(p1.getAtribute(i, choice));
+                player = p1.getAtribute(i, choice);
+                com = p2.getAtribute(i, choice);
+                System.out.println(player);
+                System.out.println(com);
+                if (player > com){
+                    System.out.println(player_name+" ganhou essa rodada");
+                }
+                if (player < com){
+                    System.out.println("COM ganhou essa rodada");
+                }
+                else{
+                    System.out.println("Empate");
+                }
             }
 
             catch (Exception e) { 
