ea236bafd6fb5e293bd20c8756d014e09486594c
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-09-24 10:27:35 -0300

    atualization of  card atributes

--- trunfo/Card.java
+++ trunfo/Card.java
@@ -3,12 +3,14 @@ import java.util.Random;
 
 public class Card {
     Random valueGenerator = new Random();
-    
-    int attack = valueGenerator.nextInt(101);
-    int defense = valueGenerator.nextInt(101);
-    int speed = valueGenerator.nextInt(101);
-    int intelligence = valueGenerator.nextInt(101);
-    int stamina = valueGenerator.nextInt(101);
+
+    // -1 was placed to avoid result 0
+    int attack = valueGenerator.nextInt(100)+1;
+    int defense = valueGenerator.nextInt(100)+1;
+    int speed = valueGenerator.nextInt(100)+1;
+    int intelligence = valueGenerator.nextInt(100)+1;
+    int dribble = valueGenerator.nextInt(100)+1;
+    int stamina = valueGenerator.nextInt(100)+1;
 
 
     public int getAttack() {
@@ -30,12 +32,16 @@ public class Card {
     public int getStamina() {
         return stamina;
     } 
+    public int getDribble() {
+        return dribble;
+    } 
     
-    public void getAllAtributes(){
+    public void getAllAtributes(){ // this function return all atributes of card, will be displayed for the user to choose the attribute he wants
         System.out.println("Attack:"+this.attack);
         System.out.println("Defense:"+this.defense);
         System.out.println("Speed:"+this.speed);
         System.out.println("Intelligence:"+this.intelligence);
+        System.out.println("Dribble:"+this.dribble);
         System.out.println("Stamina:"+this.stamina);
         System.out.println("");
     }
